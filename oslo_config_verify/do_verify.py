364f81504b10938a2b075fe201484dbb96fb7e6b
shuaijiasanshao <401708620@qq.com>
2018-06-28 21:22:42 +0800

    复现一个问题 导入两个不同的模块中同名配置项，在配置项的help描述不同下报错

---
+++ oslo_config_verify/do_verify.py
@@ -0,0 +1,4 @@
+from module_1 import app_1
+from module_2 import app_2
+
+print("end verify")
---
+++ oslo_config_verify/module_2/app_2.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+
+from oslo_config import cfg
+
+# 定义选项组
+opt_group = cfg.OptGroup(name='database',
+                         title='database configurations')
+
+# 定义选项
+common_opts = [
+    cfg.BoolOpt('debug',
+                default=False,
+                help='True means enable, False means disable.'),
+]
+
+# 创建配置类
+cf = cfg.CONF
+# 开始注册
+cf.register_group(opt_group)
+cf.register_opts(common_opts, group=opt_group)
+cf(default_config_files=['module_2/config.cfg'])
+print(cf.database.debug)
